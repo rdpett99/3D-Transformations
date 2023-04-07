44c0dc0dec432e058b6fe76190adc9b65d98120b
rdpett99 <rdp06865@uga.edu>
2023-04-07 14:49:42 -0400

    changed comment

--- transformations.py
+++ transformations.py
@@ -1,11 +1,11 @@
-import math
+from math import sin, cos, radians, sqrt
 
 # Scientific computing library for Python
 # Used for matrix multiplication operator @
 import numpy as np
 
 """
-This program contains functions that can perform 2D line
+This program contains functions that can perform 3D line
 transformations. Calculations are used by multiplying matrices.
 """
 
@@ -20,6 +20,7 @@ def translate(Tx, Ty, Tz):
                      [0,  1,  0,  0],
                      [0,  0,  1,  0],
                      [Tx, Ty, Tz, 1]])
+
 
 def scale(Sx, Sy, Sz, Cx, Cy, Cz):
     """
@@ -46,10 +47,109 @@ def scale(Sx, Sy, Sz, Cx, Cy, Cz):
 
     """
     t1_matrix = translate(-Cx, -Cy, -Cz)
+
     s_matrix = np.array([[Sx, 0,  0,  0],
                          [0,  Sy, 0,  0],
                          [0,  0,  Sz, 0],
                          [0,  0,  0,  1]])
+
     t2_matrix = translate(Cx, Cy, Cz)
 
     return t1_matrix @ s_matrix @ t2_matrix
+
+
+def rotate_x(angle):
+    """
+    Returns a rotation matrix that'll perform a rotation
+    about the x-axis, where:
+        angle : degrees in radians
+    """
+    theta = radians(angle)
+    sin0  = sin(theta)
+    cos0  = cos(theta)
+
+    return np.array([[1,   0,     0,   0],
+                     [0,  cos0,  sin0, 0],
+                     [0, -sin0,  cos0, 0],
+                     [0,   0,     0,   0]])
+
+
+def rotate_y(angle):
+    """
+    Returns a rotation matrix that'll perform a rotation
+    about the y-axis, where:
+        angle : degrees in radians
+    """
+    theta = radians(angle)
+    sin0  = sin(theta)
+    cos0  = cos(theta)
+
+    return np.array([[cos0, 0, -sin0,  0],
+                     [0,    1,   0,    0],
+                     [sin0, 0,  cos0,  0],
+                     [0,    0,   0,    1]])
+
+
+def rotate_z(angle):
+    """
+    Returns a rotation matrix that'll perform a rotation
+    about the z-axis, where:
+        angle : degrees in radians
+    """
+    theta = radians(angle)
+    sin0  = sin(theta)
+    cos0  = cos(theta)
+
+    return np.array([[cos0,  sin0, 0, 0],
+                     [-sin0, cos0, 0, 0],
+                     [0,      0,   1, 0],
+                     [0,      0,   0, 1]])
+
+
+def eyeCS_conversion(Xw, Yw, Zw, D, S):
+    """
+    Returns a transformation matrix that converts an image
+    from the World Coordinate System (WCS) to the Eye Coordinate
+    System (ECS), where:
+        Xw : x-coordinate in the WCS
+        Yw : y-coordinate in the WCS
+        Zw : z-coordinate in the WCS
+        D  : distance from the screen
+        S  : 1/2 height of the screen
+    """
+    t_matrix = translate(-Xw, -Yw, -Zw)
+
+    const_matrix = np.array([[1, 0,  0, 0],
+                             [0, 0, -1, 0],
+                             [0, 1,  0, 0],
+                             [0, 0,  0, 1]])
+
+    # magnitudes used for ry_matrix
+    ry_mag1 = Yw/sqrt(Xw**2 + Yw**2)
+    ry_mag2 = Xw/sqrt(Xw**2 + Yw**2)
+
+    ry_matrix = np.array([[-ry_mag1, 0,  ry_mag2, 0],
+                          [    0,    1,     0,    0],
+                          [-ry_mag2, 0, -ry_mag1, 0],
+                          [    0,    0,     0,    1]])
+
+    # magnitudes used for rx_matrix
+    rx_mag1 = sqrt(Xw**2 + Yw**2)/sqrt(Zw**2 + (sqrt(Xw**2 + Yw**2)**2))
+    rx_mag2 = Zw/sqrt(Zw**2 + (sqrt(Xw**2 + Yw**2)**2))
+
+    rx_matrix = np.array([[1,     0,       0,    0],
+                          [0,  rx_mag1, rx_mag2, 0],
+                          [0, -rx_mag2, rx_mag1, 0],
+                          [0,     0,       0,    1]])
+
+    rz_matrix = np.array([[1, 0,  0, 0],
+                          [0, 1,  0, 0],
+                          [0, 0, -1, 0],
+                          [0, 0,  0, 1]])
+
+    N_matrix = np.array([[D/S,  0,  0, 0],
+                         [0,   D/S, 0, 0],
+                         [0,    0,  1, 0],
+                         [0,    0,  0, 1]])
+
+    return t_matrix @ const_matrix @ ry_matrix @ rx_matrix @ rz_matrix @ N_matrix
